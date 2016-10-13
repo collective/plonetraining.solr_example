9f325aaef0eeb6a03a398e44de512ed84ca1800f
Tom Gross <itconsense@gmail.com>
2016-10-13 16:43:52 +0200

    show facets

--- src/plonetraining/solr_example/browser/views.py
+++ src/plonetraining/solr_example/browser/views.py
@@ -9,11 +9,10 @@ class FancySearchView(BrowserView):
     has_results = False
 
     def search(self):
-        catalog = api.portal.get_tool('portal_catalog')
-        SearchableText = self.request.get('SearchableText', '')
-        if not SearchableText:
+        if not self.request.get('SearchableText'):
             return []
-        results = catalog(SearchableText=SearchableText, hl='true')
+        catalog = api.portal.get_tool('portal_catalog')
+        results = catalog(hl='true', **self.request.form)
         self.has_results = bool(len(results))
         b_start = self.request.get('b_start', 0)
         batch = Batch(results, size=20, start=b_start)
