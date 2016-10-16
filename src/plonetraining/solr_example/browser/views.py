c0c4b0637dfecc6c212d4d09ee61eb2f6c475417
Tom Gross <itconsense@gmail.com>
2016-10-16 22:55:13 +0200

    add suggest part, remove unnecessary parts

--- src/plonetraining/solr_example/browser/views.py
+++ src/plonetraining/solr_example/browser/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from plone.app.contentlisting.interfaces import IContentListing
 from plone.batching import Batch
 from Products.Five import BrowserView
+from urllib import urlencode
+from zope.component import getMultiAdapter
+import json
 
 
 class FancySearchView(BrowserView):
@@ -12,8 +16,22 @@ class FancySearchView(BrowserView):
         if not self.request.get('SearchableText'):
             return []
         catalog = api.portal.get_tool('portal_catalog')
-        results = catalog(hl='true', **self.request.form)
+        results = IContentListing(catalog(hl='true', **self.request.form))
         self.has_results = bool(len(results))
         b_start = self.request.get('b_start', 0)
         batch = Batch(results, size=20, start=b_start)
         return batch
+
+    def suggest(self):
+        self.request.form['term'] = self.request.get('SearchableText')
+        suggest_view = getMultiAdapter((self.context, self.request),
+                                       name='suggest-terms')
+        suggestions = json.loads(suggest_view())
+        if suggestions:
+            word = suggestions[0]['value']['word']
+            query = self.request.form.copy()
+            query['SearchableText'] = word
+            return {'word': word,
+                    'url': '{0}?{1}'.format(self.request.getURL(),
+                                            urlencode(query, doseq=1))}
+        return ''
