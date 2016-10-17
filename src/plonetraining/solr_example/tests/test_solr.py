03081a32b9782c9d189b06679b3c2c27a1685322
Tom Gross <itconsense@gmail.com>
2016-10-17 18:19:20 +0200

    Code conventions

--- src/plonetraining/solr_example/tests/test_solr.py
+++ src/plonetraining/solr_example/tests/test_solr.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
+from collective.solr.testing import activateAndReindex
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plonetraining.solr_example.browser.views import FancySearchView
 from plonetraining.solr_example.testing import PLONETRAINING_SOLR_EXAMPLE_FUNCTIONAL_TESTING  # noqa
-from collective.solr.testing import activateAndReindex
+
 import unittest
 
 
@@ -28,5 +29,6 @@ class TestSearchView(unittest.TestCase):
         request.form['SearchableText'] = 'lore'
         self.assertEqual(
             view.suggest(),
-            {'url': 'http://nohost?term=lore&SearchableText=lorem', 'word': u'lorem'}
+            {'url': 'http://nohost?term=lore&SearchableText=lorem',
+             'word': u'lorem'}
         )
